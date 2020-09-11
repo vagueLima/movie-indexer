b15577d5e12311cc68a7636cb16f815a0bc0b767
Lucas <o.lucas.lima@gmail.com>
2020-09-11 17:04:47 -0300

    Implemented search by query

--- app.js
+++ app.js
@@ -4,21 +4,35 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 const morgan = require('morgan');
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-const MONGO_DATABASE = process.env.MONGO_DATABASE;
-
-const MONGO_URL = process.env.MONGO_URL.replace('<password>', MONGO_PASSWORD).replace(
-  '<dbname>',
-  MONGO_DATABASE
-);
-mongoose.set('useCreateIndex', true);
-mongoose.connect(MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const axios = require('axios');
+const MOVIEDB_BEARER_TOKEN = process.env.BEARER_TOKEN;
 
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
+
+app.get('/movies', function (req, res) {
+  const searchTerm = req.query.searchTerm;
+  const paramsToMovieDb = {
+    query: searchTerm,
+  };
+  const headers = { Authorization: `Bearer ${MOVIEDB_BEARER_TOKEN}` };
+  axios
+    .get('https://api.themoviedb.org/3/search/movie', {
+      params: paramsToMovieDb,
+      headers,
+    })
+    .then((response) => {
+      const prunedMovieObject = response.data.results.map((movie) => ({
+        id: movie.id,
+        original_language: movie.original_language,
+        original_title: movie.original_title,
+      }));
+      res.status(200).json(prunedMovieObject);
+    })
+    .catch((err) => {
+      res.status(500).send('Something went wrong');
+    });
+});
 app.set('port', process.env.PORT || 3000);
 
 module.exports = app;
